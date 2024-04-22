65c019e0349e6065c18547644fcbf64c04fb3245
anax3 <906120anand@gmail.com>
2024-04-23 01:16:21 +0530

    goMs

--- Microservices_go/01/main.go
+++ Microservices_go/01/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hello Prashant, this is your Go server!")
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		_, err2 := fmt.Fprintf(writer, "Hello Prashant, this is your Go server!")
+		if err2 != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+		}
+		data, err := io.ReadAll(request.Body)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			writer.Write([]byte("OOPS"))
+			return
+		}
+		fmt.Printf("Data  %s ", data)
 	})
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
